68a26ea3f7d628e45557e95229e842bb9f3110a2
Codingdn <danthedevnguyen@gmail.com>
2020-07-19 13:08:40 -0400

    created header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./App.css";
 
 //following Clever Programmer tutorial https://www.youtube.com/watch?v=1_IYL9ZMR_Y
 function App() {
   return (
-    <div className="App">
-      <h3>Hello</h3>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home Page</h1>
+          </Route>
+          </Switch>
+
+        {/**localhost.com/ */}
+        {/**localhost.com/checkout */}
+        {/**localhost.com/login */}
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      {/**logo on the left */}
+      <img src="https://www.amazon.com/ref=nav_logo" alt="amazon logo" />
+      {/**Search Box */}
+      {/**3 Links */}
+      {/**Bsaket icon with number*/}
+    </div>
+  );
+}
+
+export default Header;
