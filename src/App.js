f0affd0ac726199b1abb74ba9aac0736c19ebcd8
Codingdn <danthedevnguyen@gmail.com>
2020-07-19 22:37:36 -0400

    started working on login functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
+import Login from './Login';
 
 //following Clever Programmer tutorial https://www.youtube.com/watch?v=1_IYL9ZMR_Y
 function App() {
@@ -17,7 +18,7 @@ function App() {
             <Checkout />
           </Route>
           <Route path="/login">
-            <h1>Login</h1>
+            <Login/>
           </Route>
           <Route path="/">
             <Header />
